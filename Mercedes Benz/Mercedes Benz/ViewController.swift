b5d0be4b4b789a72dfa2c07c6fa763ed19e24786
Fernando Lopez Llanos <fernando@Fernandos-MacBook-Pro.local>
2017-08-14 13:26:47 -0700

    Cambio de texto y colores

--- Mercedes Benz/Mercedes Benz/ViewController.swift
+++ Mercedes Benz/Mercedes Benz/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var texto1: UILabel!
+    
+    @IBOutlet weak var texto2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBAction func branding(_ sender: Any) {
+        texto1.text = "Mercedes Benz"
+    }
+    
+    
+    @IBAction func color(_ sender: Any) {
+        texto2.text = "Azul"
+        texto2.textColor = UIColor(red: 0.0, green: 0.0, blue: 2.0, alpha: 1.0)
+    }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
