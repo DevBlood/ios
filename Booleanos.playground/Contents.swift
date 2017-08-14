e604b55fb1420822f5b7f80486c8d1622c320c71
Fernando Lopez Llanos <fernando@Fernandos-MacBook-Pro.local>
2017-08-14 15:56:53 -0700

    Booleanos y concatenacion

---
+++ Booleanos.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Declaracion de Booleanos
+
+var positivo:Bool = true
+var negativo:Bool = false
+
+//Operadores de asignación
+
+var x = 10
+var y = 1
+
+var comparacion = y == x
+
+comparacion
+
+// Concatenaciones
+
+let alfa:String = "ABCDEFG"
+let beto:String = "HIJKLMNOPQRSTUVWXYZ"
+
+let alfabeto = alfa + beto
+
+// String vs Character
+
+// Union de Character
+
+let char0:Character = "x"
+let char1:Character = "Y"
+
+let union2 = String (char0)
+
+//let union:Character = char0 + char1
+
+
+
+
