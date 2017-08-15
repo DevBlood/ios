9078c90fd099aee488932441a21f3ea81198741c
Fernando Lopez Llanos <fernando@Fernandos-MacBook-Pro.local>
2017-08-15 09:28:01 -0700

    Apple tv

---
+++ Clase2/BenzAppletv/BenzAppletv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BenzAppletv
+//
+//  Created by Fernando Lopez Llanos on 8/15/17.
+//  Copyright © 2017 Fernando Lopez Llanos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
