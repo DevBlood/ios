2eacf9820442d1be145bd22e97e8ec709ed5b288
Fernando Lopez Llanos <fernando@Fernandos-MacBook-Pro.local>
2017-08-16 13:29:22 -0700

    MapView

---
+++ Clase 3/MapView/MapView/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  MapView
+//
+//  Created by Fernando Lopez Llanos on 8/16/17.
+//  Copyright © 2017 Fernando Lopez Llanos. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let location = CLLocationCoordinate2D(latitude: 51.50007, longitude: -0.1246402)
+        
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Big Ben"
+        annotation.subtitle = "London"
+        mapView.addAnnotation(annotation)
+        
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
