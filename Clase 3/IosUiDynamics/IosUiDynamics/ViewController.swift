7d670aea42e0880fff4465233552012e98224e58
Fernando Lopez Llanos <fernando@Fernandos-MacBook-Pro.local>
2017-08-16 17:00:45 -0700

    Giro de imagen

--- Clase 3/IosUiDynamics/IosUiDynamics/ViewController.swift
+++ Clase 3/IosUiDynamics/IosUiDynamics/ViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
+    @IBOutlet weak var rotar: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     var animator:UIDynamicAnimator!
     var snapBehaviour:UISnapBehavior!
+    var box: UIView?
 
+//    func rotar(_ sender: Any) -> return {
+//        UIView.animate(withDuration: 2.0, animations:{
+//            self.imageView.transform = CGAffineTransform(rotationAngle: (100.0 * .pi) / 100.0)
+//        })
+//    }
+    
+   @IBAction func rotar(_ sender: Any) {
+        UIView.animate(withDuration: 2.0, animations: {
+            self.imageView.transform =
+                CGAffineTransform(rotationAngle: CGFloat.pi)
+            self.imageView.transform = CGAffineTransform(translationX: 10, y: 20)
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,9 +37,16 @@ class ViewController: UIViewController {
         self.view.addGestureRecognizer(tapGesture)
         animator = UIDynamicAnimator(referenceView: self.view)
         
-        // Do any additional setup after loading the view, typically from a nib.
+        //let frameRect = CGRect(x: 10, y: 20, width: 40, height: 40)
+        let frameRect = CGRect(x: 50, y: 200, width: 40, height: 40)
+        box = UIView(frame: frameRect)
+        box?.backgroundColor = UIColor.black
+        
+        self.view.addSubview(box!)
+        
         
     }
+    
     
     func userHasTapped(tap:UITapGestureRecognizer) {
         let touchPoint = tap.location(in: self.view)
@@ -37,7 +60,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+ 
     }
 
 
